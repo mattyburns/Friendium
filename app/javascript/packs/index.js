856fffe275760e8536e2d4e9b4e0659dc36fc1ea
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-10-23 20:39:10 -0400

    Setting up react

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../react/src/App.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('app'));
-})
