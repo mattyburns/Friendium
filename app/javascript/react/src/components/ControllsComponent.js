fd236ebf591e28a6ed3256c3a1fb5397ad66307a
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-10-29 19:27:21 -0400

    Moved nav bar elements to controlls component

--- app/javascript/react/src/components/ControllsComponent.js
+++ app/javascript/react/src/components/ControllsComponent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import AddButton from './buttons/AddButton'
 import FilterButton from './buttons/FilterButton'
+import { Link } from 'react-router'
+import BackButton from './buttons/BackButton'
 
 const ControllsComponent = props => {
 
@@ -10,6 +12,9 @@ const ControllsComponent = props => {
       <h4>I am the ControllsComponent</h4>
       <AddButton/>
       <FilterButton/>
+      <BackButton/>
+      <Link to='friend_path'>Friend Show Page</Link>
+      {props.children}
     </div>
 
 
