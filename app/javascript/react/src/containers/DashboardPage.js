9fb2efd5dd867cc03d922afa7832a46ece4c46fc
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-01 10:47:08 -0400

    Added logic to create landing page message if user is not signed in

--- app/javascript/react/src/containers/DashboardPage.js
+++ app/javascript/react/src/containers/DashboardPage.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react'
 import StatsComponent from '../components/StatsComponent'
 import FriendIndexComponent from '../components/FriendIndexComponent'
 import ControllsComponent from '../components/ControllsComponent'
+import SignInMessageTile from '../components/tiles/SignInMessageTile'
 
 
 class DashboardPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      signedIn: [],
+      currentUser: [],
       friends: []
     }
   }
 
   componentDidMount() {
+    fetch('/api/v1/user/is_signed_in.json',{
+      credentials: 'same-origin',
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({signedIn: body.signed_in})
+      this.setState({currentUser: body.user})
+    })
     fetch('/api/v1/friends', {
       credentials: 'same-origin',
       method: 'GET',
@@ -25,17 +38,22 @@ class DashboardPage extends Component {
   }
 
   render() {
+    let homePage;
+    if(this.state.signedIn == true){
+      homePage = [
+        <StatsComponent/>,
+        <ControllsComponent/>,
+        <FriendIndexComponent friends={this.state.friends}/>
+       ];
+    }else{
+      homePage = <SignInMessageTile/>
+    }
+
     return(
       <div>
         <h1>I am the DashboardPage</h1>
 
-        <StatsComponent/>
-
-        <FriendIndexComponent
-          friends={this.state.friends}
-        />
-
-        <ControllsComponent/>
+        {homePage}
 
       </div>
 
