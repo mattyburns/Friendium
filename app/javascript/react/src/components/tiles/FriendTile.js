b5c2f7280d11727dab816a1f2c13b27b065551eb
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-09 16:37:36 -0500

    Friend tile style edits

--- app/javascript/react/src/components/tiles/FriendTile.js
+++ app/javascript/react/src/components/tiles/FriendTile.js
@@ -8,18 +8,28 @@ const FriendTile = props => {
 //figure out mouse over div- change size of div
 
     <div className="friend-tile" id={props.id}>
-      <img onClick={props.handleFriendshipStats} className="avatar" id={props.id} src={face}></img>
 
-      <p>Full name: {props.fullName}</p>
-      <p>Pronouns: {props.preferredPronouns}</p>
+      <ul className="button-group radius">
+        <li><Link  className="button small" to={`/friends/${props.id}`}>Friendship History</Link>
+        </li>
+        <li>
+          <Link className="button small"  to={`/friends/${props.id}/edit`}>Edit</Link>
+        </li>
+        <li>
+          <Link className="button small" id={props.id} onClick={props.handleDeleteFriend}>Delete</Link>
+        </li>
+      </ul>
+
+      <p>{props.fullName}</p>
+      <p>{props.preferredPronouns}</p>
       <p>Contact info: {props.phoneNumber}, {props.email}</p>
 
-      <Link  className="button round" to={`/friends/${props.id}`}>Friendship History</Link>
-      <Link className="button round" id={props.id} onClick={props.handleDeleteFriend}>Delete</Link>
-      <Link className="button round"  to={`/friends/${props.id}/edit`}>Edit</Link>
 
 
-    </div>
+
+      <img onClick={props.handleFriendshipStats} className="avatar" id={props.id} src={face}></img>
+
+      </div>
   )
 }
 
