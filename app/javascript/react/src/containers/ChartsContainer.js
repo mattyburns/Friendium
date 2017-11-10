02d1d372e826936705d8fbbba298f6780866d1f1
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-10 13:17:01 -0500

    Styling for mobile version 1

--- app/javascript/react/src/containers/ChartsContainer.js
+++ app/javascript/react/src/containers/ChartsContainer.js
@@ -8,12 +8,9 @@ import ImpactDonutChart from '../components/ImpactDonutChart'
 const ChartsContainer = props =>{
   return(
     <div className="chart-container">
-      <div>
-        <ImpactDonutChart
-          data={props.data}
-        />
-      </div>
-      <h4># of days since last interaction: {props.daysSinceLastInteraction}</h4>
+      <ImpactDonutChart
+        data={props.data}
+      />
     </div>
   );
 }
