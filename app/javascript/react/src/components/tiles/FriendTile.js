0a504e0ce91454beebbf2bae01ae8a68751a9d22
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-10 09:10:35 -0500

    Edits to friend and interaction tiles, as well as buttons

--- app/javascript/react/src/components/tiles/FriendTile.js
+++ app/javascript/react/src/components/tiles/FriendTile.js
@@ -4,12 +4,15 @@ import face from '../images/face.png'
 
 const FriendTile = props => {
   return(
-
-//figure out mouse over div- change size of div
-
     <div className="friend-tile" id={props.id}>
 
-      <ul className="button-group radius">
+
+      <h3>{props.fullName}</h3>
+
+      <p>{props.preferredPronouns}</p>
+      <p>Contact info: {props.phoneNumber}, {props.email}</p>
+
+      <ul className="button-group round">
         <li><Link  className="button small" to={`/friends/${props.id}`}>Friendship History</Link>
         </li>
         <li>
@@ -19,16 +22,7 @@ const FriendTile = props => {
           <Link className="button small" id={props.id} onClick={props.handleDeleteFriend}>Delete</Link>
         </li>
       </ul>
-
-      <p>{props.fullName}</p>
-      <p>{props.preferredPronouns}</p>
-      <p>Contact info: {props.phoneNumber}, {props.email}</p>
-
-
-
-
       <img onClick={props.handleFriendshipStats} className="avatar" id={props.id} src={face}></img>
-
       </div>
   )
 }
