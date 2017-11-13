e451cdf62c24bc828a80e4861036164893f38691
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-11-13 13:06:28 -0500

    Adding Enzyme Tests

---
+++ app/javascript/react/test/ErrorsTileSpec.js
@@ -0,0 +1,16 @@
+import ErrorsTile from '../src/components/tiles/ErrorsTile';
+
+describe('ErrorsTile', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<ErrorsTile
+      errors={['error1', 'error2']}
+                    />)
+  });
+
+  it('should render an li element for each error in props', () => {
+    expect(wrapper.find('li').text()).toBe('error1')
+  });
+
+});
